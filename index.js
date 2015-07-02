b6144a5f74571578ac931a58fcce14ad676c1057
Ben Drucker <bvdrucker@gmail.com>
2015-07-02 09:47:51 -0400

    Init

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = Ear
+
+function Ear () {
+  var callbacks = []
+
+  function listeners () {
+    var args = arguments
+    var i = 0
+    var length = callbacks.length
+    for (; i < length; i++) {
+      var callback = callbacks[i]
+      callback.apply(null, args)
+    }
+  }
+
+  listeners.add = function (listener) {
+    callbacks.push(listener)
+    return function remove () {
+      var i = 0
+      var length = callbacks.length
+      for (; i < length; i++) {
+        if (callbacks[i] === listener) {
+          callbacks.splice(i, 1)
+          return
+        }
+      }
+    }
+  }
+
+  return listeners
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var test = require('tape')
+var Listeners = require('./')
+
+test(function (t) {
+  t.plan(3)
+  var listeners = Listeners()
+  listeners() // noop
+  var remove = listeners.add(function (a, b) {
+    t.equal(this, null)
+    t.equal(a, 1)
+    t.equal(b, 2)
+  })
+  listeners(1, 2)
+  remove()
+  listeners()
+})
